ee22d91787ccda0b7ca4c79859c56607b41b1df5
Michael Trottman <mtrottman@gmail.com>
2018-03-11 20:24:06 -0700

    Initial Code Commit

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        // Each member inside a class is either a State or Behavior
+        // State: Information or Variable
+        // Behavior: Function
+
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics() // Get the GPA for this GradeBook
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                // Check if this is the highest grade
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+
+                // Check if this is the lowest grade
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+
+                // Sum up the grades
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+
+        public void AddGrade(float grade) // Function which takes parameters
+        {
+            grades.Add(grade); // This will add the grade passed into the grades list
+        }
+
+        List<float> grades = new List<float>(); // List of floating point numbers
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f); // Forces this number to be a float since it would normally be a double
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+        }
+    }
+}
